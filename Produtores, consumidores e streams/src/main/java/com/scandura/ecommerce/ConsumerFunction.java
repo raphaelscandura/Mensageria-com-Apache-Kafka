54d3f1fdad07c5a5231f482d895faf3d1914e712
Raphael Scandura <raphaelscandura@hotmail.com>
2024-02-05 06:45:45 -0300

    kafka dispatcher implemented

---
+++ Produtores, consumidores e streams/src/main/java/com/scandura/ecommerce/ConsumerFunction.java
@@ -0,0 +1,7 @@
+package com.scandura.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+public interface ConsumerFunction {
+    void consume(ConsumerRecord<String, String> record);
+}
